78fd2e51c1ca97a6feec27d02b390bf6f4dafae8
thankgodandrew1 <and22046@byui.edu>
2023-12-13 11:26:33 -0700

    fixed deployment error

--- quote-generator/src/components/ErrorComponent.tsx
+++ quote-generator/src/components/ErrorComponent.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 
-const ErrorComponent = ({ error }) => {
+interface ErrorComponentsProps {
+  error: any
+}
+const ErrorComponent = ({ error }: ErrorComponentsProps) => {
   return (
     <div className='my-4 text-red-600 text-xl bg-white w-fit p-3 animate-pulse rounded-[10px] '>
       <p>{error}</p>
